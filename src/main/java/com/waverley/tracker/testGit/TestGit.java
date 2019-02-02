5c9b3a108de6473ce3fa4392dc3dee56c09c5b8d
AndyBoro <aborovoy@waverleysoftware.com>
2019-02-02 12:52:17 +0200

    Add test class

---
+++ src/main/java/com/waverley/tracker/testGit/TestGit.java
@@ -0,0 +1,11 @@
+package com.waverley.tracker.testGit;
+
+/**
+ * Created by Andrey on 2/2/2019.
+ */
+public class TestGit {
+    String test1 = "1";
+    String test2 = "2";
+    String test3 = "3";
+    String test4 = "4";
+}
